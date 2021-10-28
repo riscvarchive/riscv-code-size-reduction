2d2aae32339ab0afb799720f9b066817a8276e51
Tariq Kurd <tariqandlaura@gmail.com>
2021-10-28 14:54:30 +0100

    proofreading

--- Zce-release-candidate/build_Zces_areg_list.bat
+++
@@ -1,2 +0,0 @@
-
-asciidoctor-pdf -r asciidoctor-diagram -a mathematical-format=svg -a pdf-style=..\..\docs-templates\resources\themes\risc-v_spec-pdf.yml -a pdf-fontsdir=..\..\docs-templates\resources\fonts Zces_areg_list.adoc -o Zces_areg_list.pdf
